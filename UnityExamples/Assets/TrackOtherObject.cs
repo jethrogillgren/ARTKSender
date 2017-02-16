967cb334b553bc97e9b5c5c1f331cae1028a658c
jethrogillgren <jethrogillgren@gmail.com>
2017-02-16 22:20:16 +0000

    Important stuff, im sure

--- UnityExamples/Assets/TrackOtherObject.cs
+++ UnityExamples/Assets/TrackOtherObject.cs
@@ -6,22 +6,19 @@ public class TrackOtherObject : MonoBehaviour {
 
 	public GameObject other;
 
-	public bool alsoLookInSameDirection = false;
+	public bool trackPosition = true;
+	public bool trackRotation = true;
 
 	// Update is called once per frame
 	void LateUpdate () {
 		if (other && other.gameObject.activeInHierarchy) {
-//			this.gameObject.SetActive (true);
-			this.gameObject.transform.position = other.gameObject.transform.position;
 
-			if( alsoLookInSameDirection ) {
-//				this.gameObject.transform.forward = other.gameObject.transform.forward;
+			if (trackPosition)
+				this.gameObject.transform.position = other.gameObject.transform.position;
+
+			if( trackRotation )
 				this.gameObject.transform.rotation = other.gameObject.transform.rotation;
 
-			}
 		}
-
-
-
 	}
 }
