2b4303b36f46789116de7a68f14506e3c325b688
jethrogillgren <jethrogillgren@gmail.com>
2017-11-03 09:44:16 +0000

    Moved to 2017 and Integrated UNET basics

---
+++ Assets/NetworkDiscoveryExample/Scripts/CustomNetworkDiscovery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+	
+    public static CustomNetworkDiscovery Instance;
+
+    public Action<string, string> onServerDetected;
+
+    void OnServerDetected(string fromAddress, string data)
+    {
+        if (onServerDetected != null)
+        {
+            onServerDetected.Invoke(fromAddress, data);
+        }
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+		InitializeNetworkDiscovery();
+    }
+
+
+//    void Start()
+//    {
+//        InitializeNetworkDiscovery();
+//    }
+
+    public bool InitializeNetworkDiscovery()
+    {
+        return Initialize();
+    }
+
+    public void StartBroadcasting()
+    {
+        StartAsServer();
+    }
+
+    public void ReceiveBroadcast()
+    {
+		Debug.LogError ("J# Starting as a Client ");
+        StartAsClient();
+    }
+
+    public void StopBroadcasting()
+    {
+        StopBroadcast();
+    }
+
+    public void SetBroadcastData(string broadcastPayload)
+    {
+        broadcastData = broadcastPayload;
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        OnServerDetected(fromAddress.Split(':')[3], data);
+    }
+}
---
+++ Assets/RoomAugNetworkController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class RoomAugNetworkController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CustomNetworkDiscovery.Instance.onServerDetected += OnReceiveBraodcast;
+
+		#if (UNITY_EDITOR || UNITY_STANDALONE_OSX)
+				HostGame();
+		#elif ( UNITY_ANDROID )
+				ListenForGame();
+		#else 
+		#error not supported platform
+		#endif
+	}
+	void OnDestroy() {
+		CustomNetworkDiscovery.Instance.onServerDetected -= OnReceiveBraodcast;
+	}
+	
+	public void HostGame() {
+		Debug.LogError ("J# Hosting Game");
+
+		CustomNetworkDiscovery.Instance.StartBroadcasting();
+		NetworkManager.singleton.StartHost();
+	}
+
+	public void ListenForGame() {
+		Debug.LogError ("J# Looking for Game");
+
+		CustomNetworkDiscovery.Instance.ReceiveBroadcast();
+	}
+
+	public void JoinGame( string serverIp ) {
+
+		Debug.LogError ("J# Joining Game at " + serverIp);
+		NetworkManager.singleton.networkAddress = serverIp;
+		NetworkManager.singleton.StartClient();
+		CustomNetworkDiscovery.Instance.StopBroadcasting();
+	}
+
+	public void OnReceiveBraodcast(string fromIp, string data) {
+		JoinGame ( fromIp );
+
+	}
+}
