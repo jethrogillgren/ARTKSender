3d45bcb5baf940993dc274799c1726b50f079895
jethrogillgren <jethrogillgren@gmail.com>
2017-12-05 13:01:54 +0000

    Deer jump working in Hackspace location

--- Assets/RoomAug/Scripts/GameplayControllers/PuzzleController.cs
+++ Assets/RoomAug/Scripts/GameplayControllers/PuzzleController.cs
@@ -218,7 +218,7 @@ public class PuzzleController : NetworkBehaviour
 
 		UpdateTimeText ();
 
-		InvokeRepeating ( "HandleMinuteTimer", 1, 1 );// Seconds
+		InvokeRepeating ( "HandleMinuteTimer", 60, 60 );// Seconds
 
 	}
 
