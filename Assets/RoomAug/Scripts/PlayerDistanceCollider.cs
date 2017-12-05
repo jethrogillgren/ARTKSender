f4b72ef1f027e223223b55f696ec06779fe04542
jethrogillgren <jethrogillgren@gmail.com>
2017-12-05 11:08:11 +0000

    WIP Building Hackspace test layout

---
+++ Assets/RoomAug/Scripts/PlayerDistanceCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerDistanceCollider : MonoBehaviour {
+
+
+	public void OnTriggerEnter ( Collider collision )
+	{
+		Debug.Log ( name + " Collided with " + collision.name );
+	}
+}
